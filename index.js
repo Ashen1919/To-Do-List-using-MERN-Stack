a6944566c17f291a79a8a84daa5e1d883e652122
Ashen1919 <2303S0834@saegis.ac.lk>
2025-06-07 22:13:10 +0530

    database connection success

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import bodyParser from "body-parser";
-import express from "express"
+import express from "express";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+
+//configure dotenv
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,6 +12,15 @@ app.use(bodyParser.json());
 //Get Request
 app.get('/', (req,res) => {
     res.send('Successfully send message')
+})
+
+//database connection
+const mongourl = process.env.MONGO_URL;
+
+mongoose.connect(mongourl).then(()=> {
+    console.log('Database connected successfully')
+}).catch(() => {
+    console.log('Database connection failure')
 })
 
 //port configure

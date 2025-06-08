2c511b74399c43caffeceb7b06b62e35a2c10cf3
Ashen1919 <2303S0834@saegis.ac.lk>
2025-06-08 07:30:03 +0530

    create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+import brcypt from 'brcypt';
+import User from '../models/user';
+
+//Create an user
+export function signupUser(req,res){
+    const {email, firstName, lastName, password} = req.body
+
+    //Hash the password
+    const saltRounds = 10;
+    const hashedPassword = brcypt.hashSync(password, saltRounds);
+
+    //create new user
+    const newUser = new User({
+        email,
+        firstName,
+        lastName,
+        password : hashedPassword
+    });
+
+    newUser.save().then(() => {
+        res.status(200).json({
+            message: "User created successfully"
+        })
+    }).catch((error) => {
+        res.status(500).json({
+            message: "Fail to create an user",
+            error: error.message
+        })
+    })
+}
